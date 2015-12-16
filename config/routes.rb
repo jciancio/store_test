767cf0c37fb884a4bd5a48f6446e06fefd128865
cartagena <jcartagena93@gmail.com>
2015-12-16 15:34:24 -0500

    adds display of current items plus price before taxes at top of page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get "product/:permalink", to: "products#show", as: "product"
   post "product/:permalink", to: "products#buy", as: "buy"
   root "products#index"
+  post "product/:permalink", to: "products#buy"
+  get "basket", to: "orders#show"
 
 end
