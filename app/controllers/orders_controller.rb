79c227e4947c5d06dbe99aa304ad3848d81934fb
jciancio <jciancio7@gmail.com>
2015-12-16 14:56:27 -0500

    began writing orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,10 @@ class OrdersController < ApplicationController
   def show
   end
 
+  def checkout
+    @order = Shoppe::Order.find(current_order.id)
+  end
+
   def destroy
     current_order.destroy
     session[:order_id] = nil
