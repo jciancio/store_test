1c0f535977295aed2a9468319e2d1a218025c26d
jciancio <jciancio7@gmail.com>
2015-12-16 13:36:56 -0500

    added route to empty basket

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post 'product/:permalink', to: 'products#buy', as: 'buy'
   root 'products#index'
   get 'basket', to: 'orders#show'
+  delete 'basket', to: 'orders#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
