695e4aae1598a37460bf5105f56b51e65b4ce022
jciancio <jciancio7@gmail.com>
2015-12-16 15:17:37 -0500

    expanded checkout method as per shoppe tutorial

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,11 @@ class OrdersController < ApplicationController
 
   def checkout
     @order = Shoppe::Order.find(current_order.id)
+    if request.patch?
+      if @order.proceed_to_confirm(params[:order].permit(:first_name, :last_name, :billing_address1, :billing_address2, :billing_address3, :billing_address4, :billing_country_id, :billing_postcode, :email_address, :phone_number))
+        redirect_to checkout_payment_path
+      end
+    end
   end
 
   def destroy
