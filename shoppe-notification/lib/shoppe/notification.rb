f19eb86543d5493ee744783961e8e6c9635c9045
jciancio <jciancio7@gmail.com>
2015-12-22 16:12:40 -0500

    removed shoppe-notification gem files because i will be buidling the gem in it's own repository

--- shoppe-notification/lib/shoppe/notification.rb
+++
@@ -1,7 +0,0 @@
-require "shoppe/notification/version"
-
-module Shoppe
-  module Notification
-    # Your code goes here...
-  end
-end
--- shoppe-notification/lib/shoppe/notification/version.rb
+++
@@ -1,5 +0,0 @@
-module Shoppe
-  module Notification
-    VERSION = "0.1.0"
-  end
-end
